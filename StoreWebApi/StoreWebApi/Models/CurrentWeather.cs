45f64331bf7ab216e8098e565001b297905a1449
dariossq <73315589+dariossq@users.noreply.github.com>
2021-08-28 16:52:02 -0500

    Actualización de proyecto
    Se modifica el proyecto ya que no funcionó adecuadamente

---
+++ StoreWebApi/StoreWebApi/Models/CurrentWeather.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StoreWebApi.Models
+{
+    public partial class CurrentWeather
+    {
+        public CurrentWeather()
+        {
+            News = new HashSet<News>();
+        }
+
+        public int CurrentWeatherId { get; set; }
+        public TimeSpan? ObservationTime { get; set; }
+        public decimal? Temperature { get; set; }
+        public string WeatherDescriptions { get; set; }
+        public decimal? WindSpeed { get; set; }
+        public decimal? WindDegree { get; set; }
+        public string WindDir { get; set; }
+        public decimal? Pressure { get; set; }
+        public decimal? Precip { get; set; }
+        public decimal? Humidity { get; set; }
+        public decimal? Cloudcover { get; set; }
+        public decimal? Feelslike { get; set; }
+        public decimal? UvIndex { get; set; }
+        public decimal? Visibility { get; set; }
+
+        public ICollection<News> News { get; set; }
+    }
+}
---
+++ StoreWebApi/StoreWebApi/Models/News.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StoreWebApi.Models
+{
+    public partial class News
+    {
+        public int NewId { get; set; }
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Url { get; set; }
+        public string UrlToImage { get; set; }
+        public DateTime? PublishedAt { get; set; }
+        public string Content { get; set; }
+        public int CurrentWeatherId { get; set; }
+
+        public CurrentWeather CurrentWeather { get; set; }
+    }
+}
